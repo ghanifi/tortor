8d1580cb3f856168f8d1b262521a428c0306845f
Hanifi <guvenbas@gmail.com>
2026-06-08 20:02:03 +0300

    feat: add market_state to DEFAULT_STATE, add momentum params to config

--- src/state.js
+++ src/state.js
@@ -8,13 +8,16 @@ const DEFAULT_STATE = {
   session: { cookies: [], expires_at: null },
   positions: {},
   regime: { macro_equity: 'sideways', macro_crypto: 'sideways', updated_at: null },
+  market_state: { state: 'RISK_NEUTRAL', score: 0, last_fetch: null, previous_state: null },
   risk: { portfolio_peak_value: 0, daily_trades_today: 0, trades_paused: false, last_reset: null },
   ai_usage: { daily_calls: 0, daily_limit: 20, monthly_cost_usd: 0, monthly_budget_usd: 10, last_reset: null },
   last_check: null,
+  last_decisions: [],
   active_layer: 1
 };
 Object.freeze(DEFAULT_STATE.session);
 Object.freeze(DEFAULT_STATE.regime);
+Object.freeze(DEFAULT_STATE.market_state);
 Object.freeze(DEFAULT_STATE.risk);
 Object.freeze(DEFAULT_STATE.ai_usage);
 Object.freeze(DEFAULT_STATE);
